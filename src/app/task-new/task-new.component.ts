7f2bc430a9abbfae6bed5f2a786d048d899e26fb
Tatiana Pestova <tanya.tom106@gmail.com>
2021-03-23 22:34:52 +0300

    Update task-new.component.ts

--- src/app/task-new/task-new.component.ts
+++ src/app/task-new/task-new.component.ts
@@ -1,8 +1,9 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-task-new',
   templateUrl: './task-new.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./task-new.component.scss']
 })
 export class TaskNewComponent {
